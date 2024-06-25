e90ce919467b5811317f3578baa8e00f1773950a
savic-a <anastasijas557@gmail.com>
2024-06-25 17:58:21 +0200

    Added https for back

--- project/back/main.go
+++ project/back/main.go
@@ -17,9 +17,15 @@ func main() {
 		fmt.Println("Loading .env file error")
 	}
 
-	db, _ := config.SetupPostgres()
+	db, err := config.SetupPostgres()
+	if err != nil {
+		fmt.Println("could not set up database: %v", err)
+	}
 
 	routes.SetupRoutes(router, db)
 
-	router.Run(":9000")
+	// Run the server with HTTPS
+	if err := router.RunTLS(":443", "cert.pem", "key.pem"); err != nil {
+		fmt.Println("failed to run server: %v", err)
+	}
 }
