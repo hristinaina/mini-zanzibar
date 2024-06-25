18ed313bae321956545582534bb63b84fc8c255e
Hristina Adamovic <hristinacina@gmail.com>
2024-06-25 21:32:04 +0200

    added middleware to zanzibar

--- project/mini-zanzibar/routes/routes.go
+++ project/mini-zanzibar/routes/routes.go
@@ -1,36 +1,41 @@
 package routes
 
 import (
+	"mini-zanzibar/controllers"
+	"mini-zanzibar/middleware"
+
 	"github.com/gin-gonic/gin"
 	"github.com/hashicorp/consul/api"
 	"github.com/syndtr/goleveldb/leveldb"
-	"mini-zanzibar/controllers"
 )
 
 func SetupRoutes(r *gin.Engine, levelDB *leveldb.DB, consulDB *api.Client) {
 	levelDBRoutes := r.Group("/api/leveldb/")
 	{
 		levelDBController := controllers.NewLevelDBController(levelDB)
-		levelDBRoutes.GET("all", levelDBController.Get)
-		levelDBRoutes.GET(":key", levelDBController.GetByKey)
-		levelDBRoutes.POST("", levelDBController.Post)
-		levelDBRoutes.DELETE(":key", levelDBController.Delete)
+		middleware := middleware.NewMiddleware()
+		levelDBRoutes.GET("all", middleware.ApiKeyAuthMiddleware(), levelDBController.Get)
+		levelDBRoutes.GET(":key", middleware.ApiKeyAuthMiddleware(), levelDBController.GetByKey)
+		levelDBRoutes.POST("", middleware.ApiKeyAuthMiddleware(), levelDBController.Post)
+		levelDBRoutes.DELETE(":key", middleware.ApiKeyAuthMiddleware(), levelDBController.Delete)
 	}
 
 	consulDBRoutes := r.Group("/api/consuldb/")
 	{
+		middleware := middleware.NewMiddleware()
 		consulDBController := controllers.NewConsulDBController(consulDB)
-		consulDBRoutes.GET("all", consulDBController.Get)
-		consulDBRoutes.GET(":key", consulDBController.GetByNamespace)
-		consulDBRoutes.POST("", consulDBController.AddNamespace)
-		consulDBRoutes.DELETE(":key", consulDBController.Delete)
+		consulDBRoutes.GET("all", middleware.ApiKeyAuthMiddleware(), consulDBController.Get)
+		consulDBRoutes.GET(":key", middleware.ApiKeyAuthMiddleware(), consulDBController.GetByNamespace)
+		consulDBRoutes.POST("", middleware.ApiKeyAuthMiddleware(), consulDBController.AddNamespace)
+		consulDBRoutes.DELETE(":key", middleware.ApiKeyAuthMiddleware(), consulDBController.Delete)
 	}
 
 	aclRoutes := r.Group("/api/acl/")
 	{
+		middleware := middleware.NewMiddleware()
 		aclController := controllers.NewACLController(levelDB, consulDB)
-		aclRoutes.POST("", aclController.Add)
-		aclRoutes.PUT("", aclController.Check)
+		aclRoutes.POST("", middleware.ApiKeyAuthMiddleware(), aclController.Add)
+		aclRoutes.PUT("", middleware.ApiKeyAuthMiddleware(), aclController.Check)
 	}
 
 }
