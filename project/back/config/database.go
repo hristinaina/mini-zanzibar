510b5a5bf12a6e7d467ca2439629251e8c226563
savic-a <anastasijas557@gmail.com>
2024-06-25 21:27:13 +0200

    Implemented ssl for communication with db

--- project/back/config/database.go
+++ project/back/config/database.go
@@ -3,12 +3,21 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func SetupPostgres() (*sql.DB, error) {
-	connStr := "postgres://postgres:ftn@localhost/bezbednost?sslmode=disable"
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbSSLMode := os.Getenv("DB_SSL_MODE")
+
+	// Koristi vrednosti za kreiranje connection string-a
+	connStr := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s",
+		dbUser, dbPassword, dbHost, dbName, dbSSLMode)
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
